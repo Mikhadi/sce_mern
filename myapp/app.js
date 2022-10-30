f5ded16636efe689de59dd01599a02bde33eeb49
Mikhadi <73593882+Mikhadi@users.noreply.github.com>
2022-10-30 14:57:29 +0200

    mongoose added

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser : true })
+const db = mongoose.connection
+db.on('error', error=>{console.error(error)})
+db.once('open', ()=>{console.log('Conncected to MongoDB')})
+
 const port = process.env.PORT
 
 const postRouter = require('./routes/post_route.js')
