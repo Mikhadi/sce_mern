75a826df13a186f2418439e3e48de21ccdea7bd1
Mikhadi <73593882+Mikhadi@users.noreply.github.com>
2022-11-13 15:02:39 +0200

    typescript added

---
+++ myapp/src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
+
+import bodyParser from 'body-parser'
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb'}))
+app.use(bodyParser.json())
+
+import mongoose from 'mongoose'
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser : true })
+const db = mongoose.connection
+db.on('error', error=>{console.error(error)})
+db.once('open', ()=>{console.log('Conncected to MongoDB')})
+
+app.use(express.static('public'))
+
+
+import postRouter from './routes/post_route.js'
+
+app.use('/post', postRouter)
+
+module.exports = app
